92024f021c8e590bcb70a3bc74d45d29f97b2b98
NityaAgarwal <114755686+NityaAgarwal@users.noreply.github.com>
2024-01-27 18:27:23 +0530

    Update MergeKsortedLists.cpp

--- MergeKsortedLists.cpp
+++ MergeKsortedLists.cpp
@@ -1,5 +1,60 @@
 //https://leetcode.com/problems/merge-k-sorted-lists/
 
+// MUCH BETTER SOLUTION
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if (!list1) return list2;
+        if (!list2) return list1;
+
+        ListNode* dummy = new ListNode (-1);
+        ListNode* curr = dummy;
+
+        while (list1 && list2) {
+            if (list1->val < list2->val) {
+                curr->next = list1;
+                list1 = list1->next;
+            }
+            else {
+                curr->next = list2;
+                list2 = list2->next;
+            }
+                curr = curr->next;
+        }
+
+        if (list1) {
+            curr->next = list1;
+        }
+        if (list2) {
+            curr->next = list2;
+        }
+
+        return dummy->next;
+    }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if (lists.size() == 0) return NULL;
+        while (lists.size() > 1) {
+            vector<ListNode*> mergedLists;
+            for (int i = 0; i<lists.size(); i += 2) {
+                ListNode* list1 = lists[i];
+                ListNode* list2 = (i+1 < lists.size()) ? lists[i+1] : NULL;
+                mergedLists.push_back(mergeTwoLists(list1, list2));
+            }
+            lists = mergedLists;
+        }
+        return lists[0];
+    }
+};
 //I SOLVED THIS MYSELF - but this solution doesnt perform well time wise
 class Solution {
 public:
